75aecfee61d08b448b73a4b0f39f0a9e118c8c95
Júllio César <julliocesar5002@gmail.com>
2026-02-08 18:52:46 -0300

    Vite + Tailwind + React

---
+++ src/containers/Home/index.jsx
@@ -0,0 +1,12 @@
+import NavBar from "../../components/navBar"
+
+function Home() {
+
+    return (
+        <>
+            <NavBar />
+        </>
+    )
+}
+
+export default Home
---
+++ tailwind.confg.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        jbr_amarelo: "#B3B300",
+      },
+    },
+  },
+  plugins: [],
+};
